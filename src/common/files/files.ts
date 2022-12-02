474410d0102699e5784f3a9ee5dba543c9b1c7d4
Jack Castañeda <jackss.jccc@gmail.com>
2022-12-02 17:56:58 -0500

    feat: add function lambda file

---
+++ src/common/files/files.ts
@@ -0,0 +1,34 @@
+const AWS = require('aws-sdk');
+const parseMultipart = require('parse-multipart');
+ 
+const BUCKET = process.env.BUCKET;
+ 
+const s3 = new AWS.S3();
+ 
+exports.handler = async (event) => {
+  try {
+    const { filename, data } = extractFile(event)
+     await s3.putObject({ Bucket: BUCKET, Key: filename, ACL: 'public-read', Body: data }).promise();
+ 
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ link: `https://${BUCKET}.s3.amazonaws.com/${filename}` })
+    }
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: err.stack })
+    }
+  }
+}
+ 
+function extractFile(event) {
+  const boundary = parseMultipart.getBoundary(event.headers['Content-Type'])
+  const parts = parseMultipart.Parse(Buffer.from(event.body, 'base64'), boundary);
+  const [{ filename, data }] = parts
+ 
+  return {
+    filename,
+    data
+  }
+}
